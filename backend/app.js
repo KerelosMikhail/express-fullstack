78d5bfe6cc5375f7afcec9d2961c0a8e7f945cc1
Kerelos Mikhail <Kerelos.Adel@gmail.com>
2025-06-10 00:11:44 -0400

    Update app and port

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
+app.use((erq, res, next) => {
+  console.log("Request received!");
+  next();
+});
+
+app.use((erq, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
   res.json({ message: "Your request was successful" });
+});
+
+app.use((erq, res, next) => {
+  console.log("Response sent!");
+  next();
 });
 
 module.exports = app;
